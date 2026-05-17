3dcbe5a9a0d97fb78cfd3d9b966c79787ce3ad5a
VARTAPRAVAH Deployment <deployment@vartapravah.com>
2026-05-17 20:27:36 +0530

    fix: enforce true CBR video encoding and inject audio entropy to resolve YouTube low bitrate warnings

--- streamer/playout.sh
+++ streamer/playout.sh
@@ -35,11 +35,11 @@ while true; do
   echo "📺 [PLAYOUT] Starting live broadcast to Oracle relay..."
   # AUDIO LOCK: Injecting silent audio track (Required for YouTube to go Live)
   ffmpeg -re -f concat -safe 0 -i "$PLAYLIST" \
-    -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 \
+    -f lavfi -i anoisesrc=c=white:a=0.001:r=44100 \
     -filter_complex "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2:color=black,format=yuv420p,fps=25[v]; [0:a][1:a]amix=inputs=2:duration=first[amixout]; [amixout]volume=2[a]" \
     -map "[v]" -map "[a]" \
-    -c:v libx264 -preset ultrafast -tune zerolatency -b:v 2500k -maxrate 2500k -bufsize 5000k \
-    -threads 0 -g 50 -keyint_min 50 -x264-params "keyint=50" \
+    -c:v libx264 -preset ultrafast -tune zerolatency -b:v 2500k -minrate 2500k -maxrate 2500k -bufsize 5000k \
+    -threads 0 -g 50 -keyint_min 50 -x264-params "keyint=50:nal-hrd=cbr" \
     -c:a aac -b:a 128k -ar 44100 -shortest -f flv -flvflags no_duration_filesize \
     "$YOUTUBE_RTMP_URL"
 
